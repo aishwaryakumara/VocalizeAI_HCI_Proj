2e06147903967fedf39d27bb2376b7db1970a603
aishwaryakumara <aishwaryakumar.a4@gmail.com>
2024-11-23 00:38:58 -0500

    New comments

--- vocalizeAI_frontend/src/App.jsx
+++ vocalizeAI_frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Chat from "./components/Chat";
 
+/* The App component is a simple wrapper around the Chat component. */
+
 const App = () => {
   return <Chat />;
 };
